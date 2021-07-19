7f796b99782148595f6662cfcdbaaa6008e1699a
AutoRooter <kryptonbot@gmail.com>
2021-07-19 23:29:50 +0100

    Backend: General: Adding Redis

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -136,3 +136,12 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
+CHANNEL_LAYERS = {
+    'default' : {
+        'BACKEND' : 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
